b473663c7da6c0af69d5b0e5ac4045c32a06e513
Rajesh Rathore <rajeshrathore05011998@gmail.com>
2022-04-09 12:42:41 +0530

    feat:<Mongodb> connect mongodb

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const username = "Rajesh";
+const password = "mXVs0kYbPhtBTshw";
+const cluster = "cluster0.5gpsd";
+const dbname = "DoorSecurityDB";
+
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true     
+  }
+);
+
+const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.once("open", function () {
+    console.log("Connected successfully");
+});
+
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
